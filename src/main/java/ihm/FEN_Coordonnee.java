14eee3bb8bdee86bbf173b392a4af86d995d955e
Yann L <130548077+ShowYL@users.noreply.github.com>
2024-05-21 20:40:29 +0200

    ajout de FEN_Coordonnee.java presque terminé maanque juste les images aux moyens de paiements

---
+++ src/main/java/ihm/FEN_Coordonnee.java
@@ -0,0 +1,159 @@
+package ihm;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.GridBagLayout;
+import javax.swing.border.CompoundBorder;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+
+public class FEN_Coordonnee {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FEN_Coordonnee window = new FEN_Coordonnee();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FEN_Coordonnee() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+	    frame = new JFrame();
+	    frame.setBounds(100, 100, 450, 300);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.getContentPane().setLayout(new BorderLayout(0, 0));
+	    
+	    JPanel panelNord = new JPanel();
+	    panelNord.setBackground(new Color(0, 0, 0));
+	    frame.getContentPane().add(panelNord, BorderLayout.NORTH);
+	    
+	    JLabel lblNewLabel = new JLabel("Vos Coordonnées");
+
+
+	    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+	    int hauteur = (int) (screenSize.height * 0.7);  // 70% de la hauteur de l'écran
+	    int largueur = (int) (screenSize.width * 0.7);  // 70% de la largueur de l'écran
+	    
+	    ImageIcon icon = new ImageIcon("src\\main\\resources\\images\\icons\\Coordonnee.png");
+	    Image img = icon.getImage();
+        Image resizedImage = img.getScaledInstance(largueur/8, hauteur/8,  java.awt.Image.SCALE_SMOOTH);  
+        icon = new ImageIcon(resizedImage);
+	    lblNewLabel.setIcon(icon);
+	    lblNewLabel.setForeground(new Color(255, 255, 255));
+	    panelNord.add(lblNewLabel);
+	    
+	    JPanel panelCentre = new JPanel();
+	    frame.getContentPane().add(panelCentre, BorderLayout.CENTER);
+	    panelCentre.setLayout(new BorderLayout(0, 0));
+	    
+	    JPanel panelCentreOuest = new JPanel(new GridLayout(8,2));
+	    panelCentre.add(panelCentreOuest, BorderLayout.WEST);
+
+	    String[] labels = {"Nom : ", "Prénom : ", "Adresse 1 : ", "Adresse 2 : ", "Code Postal : ", "Ville ; ", "Téléphone : ", "Mail : "};
+
+	    for (int i = 0; i < 8; i++) {
+	        JLabel label = new JLabel(labels[i]);
+	        label.setHorizontalAlignment(JLabel.RIGHT);
+	        JTextField textField = new JTextField();
+	        panelCentreOuest.add(label);
+	        panelCentreOuest.add(textField);
+	    }
+
+	    
+	    JPanel panelCentreEst = new JPanel();
+	    panelCentre.add(panelCentreEst, BorderLayout.EAST);
+	    panelCentreEst.setLayout(new BorderLayout(0, 0));
+	    
+	    JPanel panelCentreEstNord = new JPanel();
+	    panelCentreEst.add(panelCentreEstNord, BorderLayout.NORTH);
+	    panelCentreEstNord.setBorder(BorderFactory.createLineBorder(Color.yellow, 5));
+	    panelCentreEstNord.setLayout(new BorderLayout(0, 0));
+	    
+	    JLabel lblNewLabel_1 = new JLabel("Moyen de paiement");
+	    panelCentreEstNord.add(lblNewLabel_1, BorderLayout.NORTH);
+	    
+	    JPanel panelCentreEstNordSud = new JPanel();
+	    panelCentreEstNord.add(panelCentreEstNordSud, BorderLayout.SOUTH);
+	    panelCentreEstNordSud.setLayout(new GridLayout(3,2));
+	    
+	    JRadioButton rdbtnNewRadioButton = new JRadioButton("Carte de crédit");
+	    panelCentreEstNordSud.add(rdbtnNewRadioButton);
+	    JRadioButton rdbtnNewRadioButton2 = new JRadioButton("Paypal");
+	    panelCentreEstNordSud.add(rdbtnNewRadioButton2);
+	    JRadioButton rdbtnNewRadioButton3 = new JRadioButton("Paiement par chèque");
+	    panelCentreEstNordSud.add(rdbtnNewRadioButton3);
+	    
+	    JPanel panelCentreEstSud = new JPanel();
+	    panelCentreEst.add(panelCentreEstSud, BorderLayout.SOUTH);
+	    panelCentreEstSud.setBorder(BorderFactory.createLineBorder(Color.yellow, 5));
+	    panelCentreEstSud.setLayout(new BorderLayout(0, 0));
+	    
+	    JLabel lblNewLabel_2 = new JLabel("Abonnement à notre NewsLetter");
+	    panelCentreEstSud.add(lblNewLabel_2, BorderLayout.NORTH);
+	    
+	    JPanel panelCentreEstSudSud = new JPanel();
+	    panelCentreEstSud.add(panelCentreEstSudSud, BorderLayout.SOUTH);
+	    
+	    JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Oui");
+	    panelCentreEstSudSud.add(rdbtnNewRadioButton_1);
+	    
+	    JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Non");
+	    panelCentreEstSudSud.add(rdbtnNewRadioButton_2);
+	    
+	    
+	    JPanel panelSud = new JPanel();
+	    panelSud.setBackground(new Color(0, 0, 0));
+	    frame.getContentPane().add(panelSud, BorderLayout.SOUTH);
+	    panelSud.setLayout(new BorderLayout(0, 0));
+	    
+	    JPanel panel = new JPanel();
+	    panel.setBackground(new Color(0, 0, 0));
+	    panelSud.add(panel, BorderLayout.EAST);
+	    
+	    JButton btnNewButton = new JButton("Valider");
+	    btnNewButton.setBackground(new Color(128, 255, 0));
+	    panel.add(btnNewButton);
+	    
+	    JButton btnNewButton_1 = new JButton("Annuler");
+	    btnNewButton_1.setBackground(new Color(255, 0, 0));
+	    panel.add(btnNewButton_1);
+	    
+	}
+
+
+}
