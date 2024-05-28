9d0297e1fec73368c2565677a244b31c33a968fa
Martin_BGT <martin.bagot@hotmail.com>
2024-05-28 15:20:03 +0200

    FEN_Description update

--- src/main/java/ihm/FEN_Description.java
+++ src/main/java/ihm/FEN_Description.java
@@ -30,6 +30,7 @@ public class FEN_Description extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JPanel panel;
 
 	/**
 	 * Launch the application.
@@ -51,6 +52,8 @@ public class FEN_Description extends JFrame {
 	 * Create the frame.
 	 */
 	public FEN_Description() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 685, 407);
 		contentPane = new JPanel();
@@ -60,12 +63,30 @@ public class FEN_Description extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
+		panel = new JPanel();
+		panel.setBackground(new Color(0, 0, 0));
+		panel.setForeground(new Color(0, 0, 0));
+		contentPane.add(panel, BorderLayout.NORTH);
+		
+		JLabel frommage_logo = new JLabel("");
+		
+		int h1 = (int) (screenSize.height * 0.7);  // 70% de la hauteur de l'écran	    
+		int l1 = (int) (screenSize.width * 0.7);  // 70% de la largueur de l'écran
+
+		ImageIcon icon1 = new ImageIcon("src\\main\\resources\\images\\icons\\Cheese.png");
+		Image img1 = icon1.getImage();
+		Image resizedImage1 = img1.getScaledInstance(l1/20, h1/10,  java.awt.Image.SCALE_SMOOTH);  
+		icon1 = new ImageIcon(resizedImage1);
+		
+		frommage_logo.setIcon(icon1);
+		panel.add(frommage_logo);
+		
 		JLabel titre = new JLabel("[Nom du fromage]");
-		titre.setBorder(new EmptyBorder(11, 0, 15, 0));
-		titre.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 36));
 		titre.setHorizontalAlignment(SwingConstants.CENTER);
-		titre.setForeground(new Color(255, 255, 255));
-		contentPane.add(titre, BorderLayout.NORTH);
+		titre.setForeground(Color.WHITE);
+		titre.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 36));
+		titre.setBorder(new EmptyBorder(11, 0, 15, 0));
+		panel.add(titre);
 		
 		JPanel panel_fromage = new JPanel();
 		panel_fromage.setBackground(new Color(192, 192, 192));
@@ -82,24 +103,25 @@ public class FEN_Description extends JFrame {
 		panel_fromage.add(panel_description);
 		panel_description.setLayout(new BoxLayout(panel_description, BoxLayout.Y_AXIS));
 		
-		JLabel titre_description = new JLabel("Description");
-		titre_description.setHorizontalAlignment(SwingConstants.CENTER);
-		titre_description.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		panel_description.add(titre_description);
-		
 		JTextArea description = new JTextArea();
 		description.setSelectionColor(new Color(0, 0, 0));
-		description.setForeground(new Color(0, 0, 0));
 		description.setText("TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest");
 		description.setLineWrap(true);
 		description.setWrapStyleWord(true);
-		description.setEnabled(false);
+		description.setEditable(false);
+		description.setBackground(Color.WHITE);
+	    description.setForeground(Color.BLACK);
 		
 		JScrollPane scrollPane = new JScrollPane(description);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setPreferredSize(new Dimension(200, 100));
         panel_description.add(scrollPane);
+        
+        JLabel titre_description = new JLabel("Description");
+        scrollPane.setColumnHeaderView(titre_description);
+        titre_description.setHorizontalAlignment(SwingConstants.CENTER);
+        titre_description.setFont(new Font("Tahoma", Font.PLAIN, 25));
 
 		
 		JPanel panel_footer = new JPanel();
@@ -112,16 +134,15 @@ public class FEN_Description extends JFrame {
 		panel_footer.add(panel_prix);
 		
 		JLabel euro_logo_1 = new JLabel("");
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		int hauteur = (int) (screenSize.height * 0.2);  // 70% de la hauteur de l'écran
-		int largueur = (int) (screenSize.width * 0.2);  // 70% de la largueur de l'écran
+		int h2 = (int) (screenSize.height * 0.2);  // 70% de la hauteur de l'écran
+		int l2 = (int) (screenSize.width * 0.2);  // 70% de la largueur de l'écran
 		
-		ImageIcon icon = new ImageIcon("src\\main\\resources\\images\\icons\\Euro.png");
-		Image img = icon.getImage();
-		Image resizedImage = img.getScaledInstance(largueur/8, hauteur/6,  java.awt.Image.SCALE_SMOOTH);  
-		icon = new ImageIcon(resizedImage);
+		ImageIcon icon2 = new ImageIcon("src\\main\\resources\\images\\icons\\Euro.png");
+		Image img2 = icon2.getImage();
+		Image resizedImage2 = img2.getScaledInstance(l2/8, h2/6,  java.awt.Image.SCALE_SMOOTH);  
+		icon2 = new ImageIcon(resizedImage2);
 		
-		euro_logo_1.setIcon(icon);
+		euro_logo_1.setIcon(icon2);
 		euro_logo_1.setForeground(Color.WHITE);
 		euro_logo_1.setFont(new Font("Tahoma", Font.PLAIN, 35));
 		panel_prix.add(euro_logo_1);
