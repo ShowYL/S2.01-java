9af419ba2ef4a8eedcc71ac3bb6cfc9b3c0f1281
Yann L <130548077+ShowYL@users.noreply.github.com>
2024-06-04 18:03:20 +0200

    Fenêtre panier vide terminée

--- src/main/java/ihm/FEN_Panier.java
+++ src/main/java/ihm/FEN_Panier.java
@@ -135,11 +135,9 @@ public class FEN_Panier {
 		this.Tableau_Panier.getColumnModel().getColumn(3).setResizable(false);
 		this.Tableau_Panier.getColumnModel().getColumn(4).setResizable(false);
 		this.frame.getContentPane().add(new JScrollPane(this.Tableau_Panier));
-		/*
 		for (int i=0;i<20;i++){
 			this.ajouterLigne(icon, "Fromage", 1.2F, 2);
 		}
-		*/
 		JPanel TrasporteurEtTotal = new JPanel();
 		TrasporteurEtTotal.setBackground(Constantes.GRIS_CLAIR);
 		this.frame.getContentPane().add(TrasporteurEtTotal);
